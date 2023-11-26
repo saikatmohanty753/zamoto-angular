ffce725ddcecbcf3400c040d08f969ecb361da5a
saikat373 <saikatmohanty6@gmail.com>
2023-11-26 17:32:55 +0530

    saikat first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { RestaurantItemsComponent } from './pages/restaurant-items/restaurant-items.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CategoriesComponent,
+    RestaurantItemsComponent,
+    CreateOrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  onSubmit():void{
+
+  }
+ }
